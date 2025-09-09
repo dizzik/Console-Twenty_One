edae1bf101ac622e939d1f3438cdf3c876526ad2
Alexander <sashayaremenko057@gmail.com>
2025-09-10 01:02:11 +0300

    dealerAdd

---
+++ dealer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+class dealer 
+{
+private:
+    int playerSum(std::vector<std::pair<std::string,std::string>>& myCard){
+        
+        return 0;
+    }
+
+    int successChanses(){
+        return 0;
+    }
+
+public:
+    void decision(){
+        
+    }
+};
+
